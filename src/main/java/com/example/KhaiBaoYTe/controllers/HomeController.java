7e9fd8eed1c3ca7d013c74793d40d22bb81866fe
hoangchung16-00 <hoangchung1687@gmail.com>
2021-07-23 12:42:02 +0700

    23/7/2021 commit

--- src/main/java/com/example/KhaiBaoYTe/controllers/HomeController.java
+++ src/main/java/com/example/KhaiBaoYTe/controllers/HomeController.java
@@ -1,9 +1,11 @@
 package com.example.KhaiBaoYTe.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
+    @GetMapping("/")
     public String getHome(){
         return "home";
     }
